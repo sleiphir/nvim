ff062d4fa98ca69be664bfdd0f295cb20ee3664a
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2023-07-24 12:14:32 +0200

    Add diff colors, change fugitive keybindings

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,13 +2,15 @@ require('rose-pine').setup({
     disable_background = true
 })
 
-function ColorMyPencils(color) 
+function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    vim.api.nvim_set_hl(0, "diffAdd", { bg = "#5bb899" })
+    vim.api.nvim_set_hl(0, "diffDelete", { bg = "#db6c6c" })
 end
 
 ColorMyPencils()
