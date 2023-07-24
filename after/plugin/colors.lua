e769cd88ba57f1be3fd1e6f7106cc9f4181666cf
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2023-07-24 12:39:51 +0200

    Remove custom diff colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -8,9 +8,6 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-    vim.api.nvim_set_hl(0, "diffAdd", { bg = "#5bb899" })
-    vim.api.nvim_set_hl(0, "diffDelete", { bg = "#db6c6c" })
 end
 
 ColorMyPencils()
