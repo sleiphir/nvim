3fd3e0b6bfa7e9bb9d0ce9fa2f35d25ffe0e10a6
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-04-19 03:20:40 +0200

    feat: add custom theme 'peel-enhanced'

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -12,10 +12,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    { import = "plugins" },
-}, {
-    install = {
-        colorscheme = { "fleet" },
-    },
-})
+require("lazy").setup({{ import = "plugins" }})
+
+vim.cmd([[colorscheme peel-enhanced]])
