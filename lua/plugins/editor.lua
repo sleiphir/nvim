98e35282d410dbe42365b5927d841fe23a274b0b
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-04-19 03:17:19 +0200

    feat(plugin): add folke/trouble plugin
    chore: update dependencies

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,6 +7,20 @@ return {
             { '<leader>wk', '<cmd>WhichKey<CR>', desc = 'Show key mappings' },
         },
     },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+        keys = {
+            { '<leader>xx', '<cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Trouble Workspace Diagnostics' },
+            { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Trouble Document Diagnostics' },
+            { '<leader>xl', '<cmd>TroubleToggle loclist<CR>', desc = 'Trouble Loclist' },
+            { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>', desc = 'Trouble Quickfix' },
+            { '<leader>xt', '<cmd>TroubleToggle lsp_references<CR>', desc = 'Trouble LSP References' },
+            { '<leader>xj', function() require("trouble").next({skip_groups = true, jump = true}) end, desc = 'Trouble Next' },
+            { '<leader>xk', function() require("trouble").previous({skip_groups = true, jump = true}) end, desc = 'Trouble Previous' },
+        },
+    },
     { 'wsdjeg/vim-fetch', event = "VeryLazy" },
     { 'nvim-tree/nvim-web-devicons' },
     {
