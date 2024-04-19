cbe86ded28b899087a6d3b1891d3d8d755d6c614
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-04-19 04:11:57 +0200

    feat(autocmds): add lsp auto format on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,10 @@
 --     pattern = "*",
 --     command = [[%s/\s\+$//e]],
 -- })
+
+-- LSP Auto format on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = vim.api.nvim_create_augroup("Default", {}),
+	pattern = "*",
+	command = [[lua vim.lsp.buf.format()]],
+})
