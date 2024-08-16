ac13b40808eb1701c4b2175a550a311ee938c244
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-08-16 13:44:07 +0200

    chore: disable autoformat on save
    Add terminal autocommand to enter insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,9 +5,19 @@
 --     command = [[%s/\s\+$//e]],
 -- })
 
--- LSP Auto format on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	group = vim.api.nvim_create_augroup("Default", {}),
-	pattern = "*",
-	command = [[lua vim.lsp.buf.format()]],
+-- Go into insert mode when opening a terminal pane
+vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		if vim.opt.buftype:get() == "terminal" then
+			vim.cmd(":startinsert")
+		end
+	end
 })
+
+-- LSP Auto format on save
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+-- 	group = vim.api.nvim_create_augroup("Default", {}),
+-- 	pattern = "*",
+-- 	command = [[lua vim.lsp.buf.format()]],
+-- })
