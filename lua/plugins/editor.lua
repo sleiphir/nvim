f0a845b93756c212b5856ce0cedb66ce98846251
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-11-04 14:21:29 +0100

    chore(ibl): add tab_char and show current scope more clearly

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -27,13 +27,17 @@ return {
 		opts = {},
 		config = function()
 			require("ibl").setup {
+				debounce = 100,
 				indent = {
 					char = "▏",
+					tab_char = "▏",
+					highlight = { "VertSplit" },
 				},
 				scope = {
 					enabled = true,
 					show_start = false,
 					show_end = false,
+					highlight = "LineNr",
 				},
 			}
 		end,
