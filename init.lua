e76fe9dc6588fc455b5f8d6c22e8b6a67b2cef3a
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-11-13 11:30:08 +0100

    style: set Comment highlight to regular instead of italic

--- init.lua
+++ init.lua
@@ -22,3 +22,6 @@ require("lazy").setup({ import = "custom/plugins" }, {
 })
 
 vim.cmd([[colorscheme mellow]])
+
+-- Modify Comment highlight group to remove italics
+vim.cmd([[hi Comment cterm=NONE gui=NONE]])
