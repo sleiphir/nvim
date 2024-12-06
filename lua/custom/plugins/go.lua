4a3419e02e6ea99aaf49984be75d44cb2b2ed785
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-12-06 23:02:23 +0100

    feat: add ray-x/go.nvim
    Plugin with utility functions like auto tag, auto import, etc

---
+++ lua/custom/plugins/go.lua
@@ -0,0 +1,10 @@
+return {
+	"ray-x/go.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{ "<leader>goa", "<cmd>GoAddTag<CR>", desc = "Add struct tags"},
+		{ "<leader>gor", "<cmd>GoRmTag<CR>", desc = "Remove struct tags"},
+		{ "<leader>got", "<cmd>GoAddAllTest<CR>", desc = "Add all tests"},
+	},
+}
