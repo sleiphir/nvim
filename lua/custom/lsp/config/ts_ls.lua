146291ce3be010eca4c4ba1932525c0415b82f01
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2024-12-18 12:08:15 +0100

    feat: add typescript lsp w/ volar

---
+++ lua/custom/lsp/config/ts_ls.lua
@@ -0,0 +1,7 @@
+return {
+	filetypes = {
+		"javascript",
+		"typescript",
+		"vue",
+	},
+}
---
+++ lua/custom/lsp/config/volar.lua
@@ -0,0 +1,11 @@
+return {
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    init_options = {
+        vue = {
+            hybridMode = false,
+        },
+        typescript = {
+            tsdk = "/home/simon/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/",
+        },
+    },
+}
