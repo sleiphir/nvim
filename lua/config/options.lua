c048cf66eaea8c31965999dede4c514a0d526a4f
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2026-01-06 13:02:46 +0100

    chore: update config options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,9 +6,9 @@ opt.relativenumber = true
 opt.signcolumn = 'yes'
 
 -- Use tabs
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = false
 opt.smartindent = true
 
