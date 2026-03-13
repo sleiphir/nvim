390cdfe87545e5b5485064d618f720b2ee622074
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2026-03-13 15:43:39 +0100

    fix(avante): build command

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,40 +2,26 @@ return {
 	"yetone/avante.nvim",
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	-- ⚠️ must add this setting! ! !
-	build = vim.fn.has("win32") ~= 0
-			and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-			or "make",
+	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		or "make",
 	event = "VeryLazy",
 	version = false, -- Never set this value to "*"! Never!
 	---@module 'avante'
 	---@type avante.Config
 	opts = {
-		-- add any opts here
-		-- this file can contain specific instructions for your project
 		instructions_file = "avante.md",
 		selection = {
 			enabled = true,
 			hint_display = "none",
 		},
 		-- for example
-		provider = "claude",
+		provider = "openai",
 		providers = {
-			claude = {
-				endpoint = "https://api.anthropic.com",
-				model = "claude-sonnet-4-20250514",
-				timeout = 30000, -- Timeout in milliseconds
-					extra_request_body = {
-						temperature = 0.75,
-						max_tokens = 20480,
-					},
-			},
-			moonshot = {
-				endpoint = "https://api.moonshot.ai/v1",
-				model = "kimi-k2-0711-preview",
-				timeout = 30000, -- Timeout in milliseconds
+			openai = {
+				model = "gpt-5.3-chat-latest",
 				extra_request_body = {
-					temperature = 0.75,
-					max_tokens = 32768,
+					temperature = 0,
+					max_tokens = 4096,
 				},
 			},
 		},
@@ -68,7 +54,7 @@ return {
 		},
 		{
 			-- Make sure to set this up properly if you have lazy=true
-			'MeanderingProgrammer/render-markdown.nvim',
+			"MeanderingProgrammer/render-markdown.nvim",
 			opts = {
 				file_types = { "markdown", "Avante" },
 			},
