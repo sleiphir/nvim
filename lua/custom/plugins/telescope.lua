9b37246e033fe392db8f7f71ffad27c9d2d2ad53
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2026-06-29 17:23:14 +0200

    feat(telescope): add ui-select dependency

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,19 +1,11 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	cmd = "Telescope",
 	lazy = false,
 	version = false,
 	dependencies = {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{ "nvim-lua/plenary.nvim" },
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		},
 	},
 	config = function()
 		require("custom.telescope")
